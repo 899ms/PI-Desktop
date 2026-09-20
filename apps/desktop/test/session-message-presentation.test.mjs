83ffe9d4eece6d9d6a8ec57d816655fa1e95691e
vastsa <hhxk666@gmail.com>
2026-09-21 03:21:52 +0800

    test(transcript): drop the stale search mock

--- apps/desktop/test/session-message-presentation.test.mjs
+++ apps/desktop/test/session-message-presentation.test.mjs
@@ -43,9 +43,6 @@ function loadComponent(name, extras = {}) {
     "../../../components/icons": new Proxy({}, { get: () => Icon }),
     "../../../components/ui": { TooltipButton },
     "./shared": shared,
-    "../../../lib/transcript-search-context": {
-      transcriptItemKey: (messageId, kind, roundId = "") => JSON.stringify([messageId, kind, roundId]),
-    },
     "./menu-items": { userMessageMenuItems: () => [] },
     "./TranscriptMenu": {
       useTranscriptMenu: () => () => {},
